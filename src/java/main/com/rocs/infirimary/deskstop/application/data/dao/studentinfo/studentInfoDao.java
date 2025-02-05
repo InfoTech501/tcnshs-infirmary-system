0d4f740a3a6a4a6d33f0f04983ca22cf0fa7f1e7
Neonael A Nazareno <neonael.angcaya.nazareno@gmail.com>
2025-02-05 09:36:05 +0800

    try commit

--- src/java/main/com/rocs/infirimary/deskstop/application/data/dao/studentinfo/studentInfoDao.java
+++ src/java/main/com/rocs/infirimary/deskstop/application/data/dao/studentinfo/studentInfoDao.java
@@ -3,7 +3,7 @@ package main.com.rocs.infirimary.deskstop.application.data.dao.studentinfo;
 import main.com.rocs.infirimary.deskstop.application.model.StudentInfo.studentInfo;
 
 public interface studentInfoDao {
-    studentInfo searchStudentbyLRN(long var1);
+    studentInfo searchStudentbyLRN(long LRN);
 
-    studentInfo searchStudentsbyFullName(String var1, String var2);
+    studentInfo searchStudentsbyFullName(String firstname, String lastname);
 }
